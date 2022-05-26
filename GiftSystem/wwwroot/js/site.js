9a8466f8865efc6f44fb8af029e87a7da1454e7c
djafk0 <zdravkomestendikov@gmail.com>
2022-05-26 11:57:34 +0300

    introduced datatables.net plugin for ajax table filtering

--- GiftSystem/wwwroot/js/site.js
+++ GiftSystem/wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('#myTable').DataTable({
+        "scrollY": "470px",
+        "scrollCollapse": true,
+        "paging": true
+    });
 
-// Write your JavaScript code.
+    $('#myTable').removeClass('d-none');
+    $('#sendGiftButton').removeClass('d-none'); 
+});
